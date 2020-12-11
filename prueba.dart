6ea710bb873edc717132262ad8d70cbee33b58be
GasparMelchor <gaspar.caamalaban@itsva.edu.mx>
2020-12-11 16:40:42 -0600

    Create prueba.dart
    archivo de prueba de funcionamiento de tokens

---
+++ prueba.dart
@@ -0,0 +1,14 @@
+var edad = 31;
+  var nombre = "Flutter";
+  int ano_nacimiento = 31;
+  var pi= 3.141516;
+  
+  int num1 = 134;
+  int num2 = 121;
+  
+  print(edad);
+  print(num1 + num2); 
+  print(num1 - num2);
+  print(num1 * num2);
+  print(num1 / num2);
+  print(num1 % num2);
